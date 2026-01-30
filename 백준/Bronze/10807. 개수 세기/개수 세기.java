560ad8d247b7fbf060bbccb0d9adf9ebaccd47c9
Karryun <165464282+Karryun@users.noreply.github.com>
2026-01-30 21:10:34 +0900

    [Bronze V] Title: 개수 세기, Time: 160 ms, Memory: 17832 KB -SsafyToday

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,29 @@
+import java.awt.Frame;
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int sum = 0;
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int v = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			if (arr[i] == v) {
+				sum += 1;
+			}
+		}
+		System.out.println(sum);
+		
+		
+		}
+
+	}
+
