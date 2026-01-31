e5be86b1d7b603aafb80696d0877e5af4e21f301
Karryun <165464282+Karryun@users.noreply.github.com>
2026-01-31 10:35:47 +0900

    [Bronze V] Title: 아스키 코드, Time: 180 ms, Memory: 17536 KB -SsafyToday

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,18 @@
+import java.awt.Frame;
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		char ch = n.charAt(0);
+		
+		System.out.println((int)ch);
+		
+		
+		}
+
+	}
+
