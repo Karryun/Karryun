c764d8407ffd0782571c3be935c24f303db22b2a
Karryun <165464282+Karryun@users.noreply.github.com>
2026-02-18 19:13:38 +0900

    [D2] Title: 등산로, Time: 113  ms, Memory: 29,312  KB -SsafyToday

--- SWEA/D2/25052. 등산로/README.md
+++ SWEA/D2/25052. 등산로/README.md
@@ -4,10 +4,10 @@
 
 ### 성능 요약
 
-메모리: 28,800  KB, 시간: 113  ms, 코드길이: 1,420 Bytes
+메모리: 29,312  KB, 시간: 113  ms, 코드길이: 1,771 Bytes
 
 ### 제출 일자
 
-2026-02-16 13:13
+2026-02-18 19:13
 
 > 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
--- SWEA/D2/25052. 등산로/등산로.java
+++ SWEA/D2/25052. 등산로/등산로.java
@@ -1,7 +1,7 @@
-import java.awt.image.Kernel;
 import java.util.Scanner;
 
 public class Solution {
+
 	// 상 하 좌 우
 	static int[] updown = { -1, 1, 0, 0 };
 	static int[] side = { 0, 0, -1, 1 };
@@ -13,7 +13,7 @@ public class Solution {
 		for (int tc = 1; tc <= t; tc++) {
 			int n = sc.nextInt();
 			int[][] arr = new int[n][n];
-			int ans = 0;
+			int ans = Integer.MIN_VALUE; // 가장 긴 등산로의 길이
 
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
@@ -22,46 +22,40 @@ public class Solution {
 			}
 
 			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					int gizunRow = i;
-					int gizunCol = j;
+				for (int j = 0; j < n; j++) { // 모든 좌표 다 탐색 예정
 					int len = 1;
-					while (true) {
-						int gizun = arr[gizunRow][gizunCol];
-						int tmpRow = i;
-						int tmpCol = j;
-						int tmpMin = Integer.MAX_VALUE;
-						boolean find = false;
-
+					int gizunRow = i; // 상하좌우 기준이 될 행
+					int gizunCol = j; // 상하좌우 기준이 될 열
+					int gizunMin = arr[i][j]; // 상하좌우 기준이 될 값 (이것보다 무조건 작아야됨)
+					int tmpRow = 0;
+					int tmpCol = 0;
+					int tmpMin = gizunMin; // 여기에 상하좌우 조사한 값 중 가장 작은 값 넣을 예정
+					
+					while (true) { // 등산로 조건 만족하는 끝까지 돌기
 						for (int k = 0; k < 4; k++) {
 							int nr = gizunRow + updown[k];
 							int nc = gizunCol + side[k];
-
-							if (nr >= 0 && nr < n && nc >= 0 && nc < n) {
-								if (gizun > arr[nr][nc]) {
-									if (tmpMin > arr[nr][nc]) {
-										tmpRow = nr;
-										tmpCol = nc;
-										tmpMin = arr[nr][nc];
-										find = true;
-									}
+							
+							if (nr >= 0 && nr < n && nc >= 0 && nc < n) { // 만족하는 값일 때
+								// 상하좌우 조사한 값들보다 작아야함!
+								if (tmpMin > arr[nr][nc]) {
+									tmpMin = arr[nr][nc]; // 반복문 끝나면 이게 그 다음으로 갈 기준
+									tmpRow = nr; 
+									tmpCol = nc; 
 								}
-							}
+							}	
 						}
+						// 상하좌우 다 돌아봤는데 기준보다 작은 값이 없는 경우니 나오기
+						if (gizunMin == tmpMin) break;
 						gizunRow = tmpRow;
 						gizunCol = tmpCol;
-						gizun = tmpMin;
+						gizunMin = tmpMin;
 						len++;
-
-						if (!find) {
-							ans = Math.max(ans, len);
-							break;
-						}
-					}
-
-				}
+					} // 여기까지 while 문
+					ans = Math.max(ans, len);
+				} // 여기까지 한 좌표에 대한 탐색 끝내야 함
 			}
-			System.out.println("#" + tc + " " + (ans - 1));
+			System.out.println("#" + tc + " " + ans);
 		}
 	}
 }
