e9e39c8210258452b67d6d495f8ac51dc7c4fdcc
Karryun <165464282+Karryun@users.noreply.github.com>
2026-02-26 22:21:40 +0900

    [Bronze II] Title: 단어의 개수, Time: 488 ms, Memory: 62556 KB -SsafyToday

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine().trim();
+		String[] arr = s.split(" ");
+		
+		if (arr[0] == "") System.out.println(0);
+		else System.out.println(arr.length);
+	}
+}
